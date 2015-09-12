463d0e31c40fbeb697c646f65da7096749ad3fa9
thunderenlight <thunderenlight@gmail.com>
2015-09-11 19:21:09 -0500

    Updating controllers and views

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,59 @@
+class KittensController < ApplicationController
+	respond_to :html, :json
+
+	def index
+		@kittens = Kitten.all
+	end
+
+
+	def create
+		@kitten = Kitten.new(kitten_params)
+		if @kitten.save
+			flash[:success] = "Kitten #{@kitten.name} has been created!"
+			redirect_to @kitten
+		else
+			render 'new'
+		end
+	end
+
+	def new
+		@kitten = Kitten.new 
+	end
+
+
+
+	def edit
+		@kitten = Kitten.find(params[:id])
+	end
+
+	def show
+		@kitten = Kitten.find(params[:id])
+	end
+
+	def update
+		@kitten = Kitten.find(params[:id])
+		if @kitten.update(kitten_params)
+			flash[:sucesss] = "Kitten #{@kitten.name} has been updated!"
+			redirect_to @kitten
+		else
+			render 'edit'
+		end
+
+	end
+
+
+	def destroy
+		@kitten = Kitten.find(params[:id])
+		@kitten.delete
+		flash.now[:notice] = "Are you certain, you really want to remove a helpless kitten?"
+		redirect_to(root_path)
+
+	end
+
+	private
+
+	def kitten_params
+		params.require(:kitten).permit(:name, :age, :cuteness, :softness, :toughness, :description)
+	end
+
+end
